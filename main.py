0e45c8c358265d4dd34c20ad0add1b25b47a80ec
bolsonitro <bolsonitro_bitcoiner@protonmail.com>
2022-12-23 13:23:09 -0300

    Resolve bug do texto na tela
    Os campos não devem ser redeclarados na função printInfos()

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ WIFI_PASSWORD = ''
 
 # Chama as funções e imprime as informações na tela
 def printInfos():
-  
-  BlockLabel = M5TextBox(14, 40, "Block: Loading", lcd.FONT_Ubuntu, 0xFFFFFF, rotate=0)
-  title0 = M5Title(title="Dados do Bitcoin", x=3, fgcolor=0xFFFFFF, bgcolor=0x0032FF)
-  PriceLabel = M5TextBox(14, 62, "Price: Loading", lcd.FONT_Ubuntu, 0xFFFFFF, rotate=0)
-  TransactionLabel = M5TextBox(14, 86, "Transactions: Loading", lcd.FONT_Ubuntu, 0xFFFFFF, rotate=0)
-  
   BlockLabel.setText(str((str('Block: ') + str(getBlock()))))
   PriceLabel.setText(str((str('Price: R$ ') + str((getBitcoinData()['price'])))))
   TransactionLabel.setText('Transactions: '+ str(getTransactions()))
@@ -65,4 +59,4 @@ wifiCfg.doConnect(WIFI_SSID, WIFI_PASSWORD)
 while True:
   Battery.setText(str(map_value((axp.getBatVoltage()), 3.7, 4.1, 0, 100)) + "%")
   printInfos()
-  wait(10000)
+  wait(10) # 10 segundos
